a32f848cbc0a84ae645db10a8f6b7120c0bdc084
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-01 21:08:05 +0100

    Use express routers to define user and task endpoints on independent files

---
+++ src/routers/task.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const Task = require('../models/task');
+
+const router = new express.Router();
+
+router.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findById(id);
+
+    if (!task) {
+      res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch('/tasks/:id', async (req, res) => {
+  const { params, body } = req;
+  const { id } = params;
+  const updates = Object.keys(body);
+  const allowedUpdates = ['description', 'completed'];
+
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    res.status(400).send({ error: 'Invalid update' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(id, body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
