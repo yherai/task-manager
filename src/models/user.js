aa3b7a3efca0bedeb528da809e0fcadbc5b53331
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-02 09:40:28 +0100

    Create an endpoint to login in the application
    This endpoint will validate the email and password
    
    Email attribute in User document now must be unique

--- src/models/user.js
+++ src/models/user.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -37,6 +38,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to logim');
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving it
 userSchema.pre('save', async function (next) {
   const user = this;
 
