83e7d78f5d6514defaf7e786be0f0e49d44216e5
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-06 09:02:34 +0100

    Include pagination for get task endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -26,7 +26,16 @@ router.get('/tasks', auth, async (req, res) => {
   }
 
   try {
-    await req.user.populate({ path: 'tasks', match }).execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send(error);
