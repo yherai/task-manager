81e8a0593dcc6261abb42db6132f00a5629f010b
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-06 10:09:08 +0100

    Create an endpoint to delete avatar image

--- src/routers/user.js
+++ src/routers/user.js
@@ -123,4 +123,16 @@ router.post(
   }
 );
 
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+
+  try {
+    await req.user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
