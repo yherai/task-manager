d7a6b33730de5cb13e04cc1909519bfafb5eafc5
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-06 20:34:56 +0100

    Include user welcome and cancelation email sending

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
-const router = new express.Router();
 const multer = require('multer');
 const sharp = require('sharp');
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account');
+const router = new express.Router();
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
 
     res.status(201).send({ user, token });
@@ -88,6 +90,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
 
     res.send(req.user);
   } catch (error) {
